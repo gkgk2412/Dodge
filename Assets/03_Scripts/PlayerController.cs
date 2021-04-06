c757138085ecd6f0d5d193571cfe20aefd671b0d
HuiJin son <54790406+gkgk2412@users.noreply.github.com>
2021-04-06 19:51:50 +0900

    .
    .

--- Assets/03_Scripts/PlayerController.cs
+++ Assets/03_Scripts/PlayerController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 //사용자의 키보드 입력을 감지하여 플레이어 이동
 
 public class PlayerController : MonoBehaviour
-{    
-    public Rigidbody rb;        //이동에 사용할 리지드바디 컴포넌트
+{
+    private Rigidbody rb;        //이동에 사용할 리지드바디 컴포넌트
     public float speed = 8.0f;  //속도
 
     private void Start()
@@ -25,19 +25,19 @@ public class PlayerController : MonoBehaviour
         //down
         if (Input.GetKey(KeyCode.DownArrow) == true)
         {
-            rb.AddForce(0f, 0f, speed);
+            rb.AddForce(0f, 0f, -speed);
         }
 
         //left
         if (Input.GetKey(KeyCode.LeftArrow) == true)
         {
-            rb.AddForce(0f, 0f, speed);
+            rb.AddForce(-speed, 0f, 0f);
         }
 
         //right
         if (Input.GetKey(KeyCode.RightArrow) == true)
         {
-            rb.AddForce(0f, 0f, speed);
+            rb.AddForce(speed, 0f, speed);
         }
     }
 
